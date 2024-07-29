ced02ed975fda3800e0a5538a776c00d4ee8671c
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-07-29 11:45:51 +0530

    ArrayAddition.py
    Today try array basic programs

---
+++ ArrayAddition.py
@@ -0,0 +1,16 @@
+def ArrayAddition(l) :
+
+       n=len(l)
+       sum=0
+
+       for i in range(n) :
+        
+             sum+=l[i]
+
+       print(sum)
+
+
+l=[12,34,5,6,7,1]
+
+ArrayAddition(l)
+
