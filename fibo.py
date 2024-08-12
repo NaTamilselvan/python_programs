00af28b307018f9be67497d6f735d2c8b3823f7c
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-12 10:15:44 +0530

    Add files via upload
    Today workout start fibanoci number

---
+++ fibo.py
@@ -0,0 +1,10 @@
+#This program is for fibonaci
+
+def fibonacci(n):
+       f=0
+      s=1      
+     for i in range(n):
+             print(f)
+            thired=f+s
+            f=s
+           s=th
