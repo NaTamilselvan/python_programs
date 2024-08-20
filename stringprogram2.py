69fa4c30c2927ba01930caee704854f60975b99b
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-20 18:56:05 +0530

    Add files via upload
    this program trying to insert string

---
+++ stringprogram2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 06:40:53 2024
+
+@author: ELCOT
+"""
+
+
+str1='Hi i am python developer'
+
+str2="Hi i am python developer"
+
+str3='''Hi i am python developer'''
+
+
+#print(str1)
+#print(str2)
+#print(str3)
+
+
+st='i am trying to 'inside a quote'
