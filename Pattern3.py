833a54714bc616172c77bbe0685c2f71922b0543
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-23 15:25:31 +0530

    Pattern3.py

---
+++ Pattern3.py
@@ -0,0 +1,26 @@
+package Pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<5;j++) {
+				
+				if(i==0 || i==4 ||j==0||j==4) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+				
+				
+				
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
