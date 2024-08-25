e5774e16455bbb19a75f7c33c2d8aabe8de64626
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-25 14:48:17 +0530

    Add files via upload

---
+++ reversestringtrial2.py
@@ -0,0 +1,19 @@
+
+a=[10,20,30,40,50,60]
+
+
+
+r=int(input("Enter the rotation time"))
+
+
+for i in range(r):
+
+     k=a.pop(0)
+            
+     k=a.append(k)       
+            
+     
+        
+     
+        
+print(a)     
