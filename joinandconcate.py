83eec85c71b47cb492884226ed828c637de869fa
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-29 17:17:19 +0530

    Add files via upload

---
+++ joinandconcate.py
@@ -0,0 +1,19 @@
+#s=input("Enter the String\n")
+
+
+list=["c","java","python","Machinelearning"]
+
+s=""
+
+s2=""
+for i in list:
+    s=s+i
+    
+
+
+    
+s2=s2.join(list)
+    
+print(s)    
+
+print(s2)
