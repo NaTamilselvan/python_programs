b68afb2027e4995e87cf0d4c0ce3242ef9624e15
NaTamilselvan <168749950+NaTamilselvan@users.noreply.github.com>
2024-08-29 19:04:38 +0530

    Add files via upload

---
+++ Swapcase.py
@@ -0,0 +1,9 @@
+
+
+s=input("Enter the String\n")
+
+
+s2=s.swapcase()
+
+
+print(s2)
---
+++ endhand.py
@@ -0,0 +1,13 @@
+
+#i have a one string list its contain no of string list 
+#To check each string end value is equal or not 
+
+
+lst=["https://google.com","http://yahoo.com","https.youtube.com","httpas.education.org","https.hhhh.org"]
+
+
+for i in lst:
+    
+    if  i[len(i)-3:]=="com":
+         print(i)
+        
